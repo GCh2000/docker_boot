68cded494dcaa528a49783f42026f6c9762d658c
GCh2000 <1293947641@qq.com>
2024-06-14 21:27:26 +0800

    first commit

---
+++ src/main/java/com/atguigu/docker/controller/OrderController.java
@@ -0,0 +1,30 @@
+package com.atguigu.docker.controller;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.UUID;
+
+/**
+ * ClassName: OrderController
+ * Package: com.atuigu.docker.controller
+ * Description:
+ *
+ * @Author: 管成豪
+ * @Create: 2024/6/8 - 0:52
+ * @Version: v1.0
+ */
+@RestController
+public class OrderController{
+    @Value("${server.port}")
+    private String port;
+    @RequestMapping("/order/docker")
+    public String helloDocker()
+    {
+        return "hello docker"+"\t"+port+"\t"+ UUID.randomUUID().toString();
+    }
+    @RequestMapping(value ="/order/index",method = RequestMethod.GET)
+    public String index()
+    {
+        return "服务端口号: "+"\t"+port+"\t"+UUID.randomUUID().toString();
+    }}
